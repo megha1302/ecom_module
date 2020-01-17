78b6598f9b31b88b86bf2d367d0fcdf23828100a
megha1302 <megha.sonkusare@anchanto.com>
2020-01-17 11:15:29 +0530

    client side form validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,5 @@ class Order < ApplicationRecord
   has_many :orderproducts, dependent: :destroy
   has_many :products, through: :orderproducts
   accepts_nested_attributes_for :orderproducts
-  accepts_nested_attributes_for :products
- 
-   
+  accepts_nested_attributes_for :products       
 end
